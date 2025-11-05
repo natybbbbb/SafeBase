ef4c3b575685ba78a95b02fffa4731028b4227c1
natybbbbb <baburovaanatalya@gmail.com>
2025-11-05 10:36:39 +0100

    feat: add SafeBaseV2 implementation with reinitializer for upgrade demo

---
+++ contracts/SafeBaseV2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract SafeBaseV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 private _value;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initializeV2() public reinitializer(2) {
+        _value = _value + 1;
+    }
+
+    function initialize(uint256 initialValue, address owner_) public initializer {
+        __Ownable_init(owner_);
+        __UUPSUpgradeable_init();
+        _value = initialValue;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function getValue() external view returns (uint256) {
+        return _value;
+    }
+
+    function setValue(uint256 newValue) external onlyOwner {
+        _value = newValue;
+    }
+}
